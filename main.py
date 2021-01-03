ce336c5577b63d04d81c4996d2f0183dd2e1be10
Demongo2009 <kuba.galat1@wp.pl>
2021-01-03 15:53:58 +0100

    trying to make DEANN better, added constraint violation penalty

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import random
+import pyDOE
+from pyDOE import lhs
 import math
 import numpy as np
 from cec17_functions import cec17_test_func
@@ -23,9 +25,9 @@ def func(y):
 
 class DEParams:
     populationSize = 100
-    mutationFactor = 0.1
     crossoverProbability = 0.9
     differentialWeight = 0.8
+    penaltyFactor = 0.1
     maxfes = 200000
     evaluationFunction = staticmethod(cec17_test_func)
 
@@ -51,6 +53,23 @@ def crossover(specimen, donorVector, params):
     return trialVector
 
 
+def constraintViolation(x):
+    minViolation = minValue - x
+    maxViolation = x - maxValue
+    if minViolation > maxViolation:
+        value = minViolation
+    else:
+        value = maxViolation
+
+    return value
+
+
+def penalty(vector, params):
+    arr = np.array([ params.penaltyFactor * max( 0, constraintViolation(x) ) for x in vector ])
+    penaltyValue = arr.sum()
+    return penaltyValue
+
+
 def evaluate(y, x, population, params):
     global dimensions
     global funNumCEC
@@ -58,6 +77,8 @@ def evaluate(y, x, population, params):
     y_val = [0]
     params.evaluationFunction(x, x_val, dimensions, 1, funNumCEC)
     params.evaluationFunction(y, y_val, dimensions, 1, funNumCEC)
+    x_val += penalty(x, params)
+    y_val += penalty(y, params)
     if y_val <= x_val:
         population[np.where( population == x )[0][0]] = y
 
@@ -85,16 +106,20 @@ def DE(params):
 
 
 class DEANNParams:
-    populationSize = 100
-    mutationFactor = 0.1
-    crossoverProbability = 0.9
+    populationSize = 50
+    crossoverProbability = 0.7
     differentialWeight = 0.8
-    maxfes = 20000
-    trainingDataSize = 10000
+    penaltyFactor = 0.1
+    maxfes = 1000
+    trainingDataSize = 1000
     evaluationFunction = staticmethod(cec17_test_func)
 
 def generateTrainingData(params):
     data = np.random.uniform(minValue, maxValue, (params.trainingDataSize, dimensions))
+    # lhsData = lhs(dimensions, samples=params.trainingDataSize)
+    # print(lhsData)
+    # data = np.array(lhsData) * 200
+    # data = data - 100
     cut = np.int32(params.trainingDataSize * 0.8)
     return data[:cut, :], data[cut:, :]
 
@@ -117,8 +142,12 @@ def evaluateSet(set, params):
         np.append(evaluated,val)
     return evaluated
 
-def evaluateWithModel(y, x, population, model):
-    if model.predict(np.array(y).reshape(1,dimensions)) <= model.predict(np.array(x).reshape(1,dimensions)):
+def evaluateWithModel(y, x, population, model, params):
+    x_val = model.predict(np.array(x).reshape(1,dimensions))
+    y_val = model.predict(np.array(y).reshape(1,dimensions))
+    x_val += penalty(x, params)
+    y_val += penalty(y, params)
+    if y_val <= x_val:
         population[np.where( population == x )[0][0]] = y
 
 
@@ -139,26 +168,27 @@ def DEANN(params):
     inputs = keras.Input(shape=(dimensions))
 
     x = normalizer(inputs)
-    x = layers.Dense(50, activation='relu')(x)
-    x = layers.Dense(50, activation='relu')(x)
-    x = layers.Dense(50, activation='relu')(x)
+    x = layers.Dense(20, activation=keras.activations.relu, kernel_regularizer=l1_l2(0.1), bias_regularizer=l1_l2(0.1))(x)
+    x = layers.Dense(20, activation=keras.activations.relu, kernel_regularizer=l1_l2(0.1), bias_regularizer=l1_l2(0.1))(x)
+    x = layers.Dense(20, activation=keras.activations.relu, kernel_regularizer=l1_l2(0.1), bias_regularizer=l1_l2(0.1))(x)
 
-    outputs = layers.Dense(1, activation='relu')(x)
+
+    outputs = layers.Dense(1, activation=keras.activations.linear)(x)
 
     model = keras.Model(inputs=inputs, outputs=outputs)
 
     model.compile(optimizer=keras.optimizers.Adam(), loss=keras.losses.MeanSquaredError())
 
-    model.fit(training, evaluatedTraining, batch_size=np.int32(params.trainingDataSize * 0.8), epochs=1000, validation_data=(validation,evaluatedValidation))
+    model.fit(training, evaluatedTraining, batch_size=np.int32(params.trainingDataSize * 0.8), epochs=1000, validation_data=(validation, evaluatedValidation))
 
 
     fes = 0
     while fes < params.maxfes:
         for specimen in population:
-            individuals = generate(population, np.where( population == specimen)[0][0], params)
+            individuals = generate(population, np.where( population == specimen )[0][0], params)
             donorVector = mutation(individuals, params)
             trialVector = crossover(specimen, donorVector, params)
-            evaluateWithModel(trialVector, specimen, population, model)
+            evaluateWithModel(trialVector, specimen, population, model, params)
             fes += 1
         generationNum += 1
     return population
@@ -178,6 +208,9 @@ if __name__ == '__main__':
     generationNum = 0
     dimensions = 10  #only: 2, 10, 20, 30, 50, 100
     funNumCEC = 1
+
+    random.seed(42)
+    np.random.seed(42)
 
     dEParams = DEParams()
     population = DE(dEParams)
