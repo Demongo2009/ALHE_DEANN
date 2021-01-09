ff5e0aabddfd16a340f2d4ff35d651b643b00036
kkulesz <kulesza.konrad@protonmail.com>
2021-01-09 13:08:10 +0100

    change project structure

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import random
 import pyDOE
 from pyDOE import lhs
@@ -15,196 +10,178 @@ from tensorflow.keras.layers.experimental.preprocessing import Normalization
 from tensorflow.keras import layers
 from tensorflow.keras.regularizers import l1_l2
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-def func(y):
-    return sum([ x**2 for x in y ])
 
 class DEParams:
-    populationSize = 100
+    populationSize = 101
     crossoverProbability = 0.9
     differentialWeight = 0.8
     penaltyFactor = 0.1
-    maxfes = 200000
+    maxfes = 2000
     evaluationFunction = staticmethod(cec17_test_func)
 
-
-def initialization(populationSize):
-    return np.random.uniform(minValue, maxValue, (populationSize, dimensions))
-
-
-def generate(population, index, params):
-    indexSet = { index }
-    individuals = [ population[i] for i in random.sample( set(range(params.populationSize)) - indexSet, 3 ) ]
-    return individuals
+    #for surogate model
+    trainingDataSize = 1000
 
 
-def mutation(individuals, params):
-    donorVector = np.add(individuals[0], params.differentialWeight * np.array(np.subtract(individuals[1], individuals[2])))
-    return donorVector
+class Common:
+    @staticmethod
+    def initialization(populationSize):
+        return np.random.uniform(minValue, maxValue, (populationSize, dimensions))
+
+    @staticmethod
+    def generate(population, index, params):
+        indexSet = { index }
+        individuals = [ population[i] for i in random.sample( set(range(params.populationSize)) - indexSet, 3 ) ]
+        return individuals
+
+    @staticmethod
+    def mutation(individuals, params):
+        donorVector = np.add(individuals[0], params.differentialWeight * np.array(np.subtract(individuals[1], individuals[2])))
+        return donorVector
+
+    @staticmethod
+    def crossover(specimen, donorVector, params):
+        randomI = random.sample(range(dimensions),1)
+        trialVector = [ donorVector[i] if random.uniform(0,1) <= params.crossoverProbability or i == randomI else specimen[i] for i in range(dimensions) ]
+        return trialVector
+
+    @staticmethod
+    def constraintViolation(x):
+        minViolation = minValue - x
+        maxViolation = x - maxValue
+        if minViolation > maxViolation:
+            value = minViolation
+        else:
+            value = maxViolation
+
+        return value
+
+    @staticmethod
+    def penalty(vector, params):
+        arr = np.array([ params.penaltyFactor * max( 0, Common.constraintViolation(x) ) for x in vector ])
+        penaltyValue = arr.sum()
+        return penaltyValue
+
+class DE:
+    @staticmethod
+    def evaluate(y, x, population, params):
+        global dimensions
+        global funNumCEC
+        x_val = [0]
+        y_val = [0]
+        params.evaluationFunction(x, x_val, dimensions, 1, funNumCEC)
+        params.evaluationFunction(y, y_val, dimensions, 1, funNumCEC)
+        x_val += Common.penalty(x, params)
+        y_val += Common.penalty(y, params)
+        if y_val <= x_val:
+            population[np.where( population == x )[0][0]] = y
 
 
-def crossover(specimen, donorVector, params):
-    randomI = random.sample(range(dimensions),1)
-    trialVector = [ donorVector[i] if random.uniform(0,1) <= params.crossoverProbability or i == randomI else specimen[i] for i in range(dimensions) ]
-    return trialVector
+    def run(self, params):
+        global generationNum
+        population = Common.initialization(params.populationSize)
+        fes = 0
+        while fes < params.maxfes:
+            for specimen in population:
+                individuals = Common.generate(population, np.where( population == specimen )[0][0], params)
+                donorVector = Common.mutation(individuals, params)
+                trialVector = Common.crossover(specimen, donorVector, params)
+                DE.evaluate(trialVector, specimen, population, params)
+                fes+=1
+            generationNum+=1
+        return population
 
+class DEANN:
+    @staticmethod
+    def generateTrainingData(params):
+        data = np.random.uniform(minValue, maxValue, (params.trainingDataSize, dimensions))
+        # lhsData = lhs(dimensions, samples=params.trainingDataSize)
+        # print(lhsData)
+        # data = np.array(lhsData) * 200
+        # data = data - 100
+        cut = np.int32(params.trainingDataSize * 0.8)
+        return data[:cut, :], data[cut:, :]
 
-def constraintViolation(x):
-    minViolation = minValue - x
-    maxViolation = x - maxValue
-    if minViolation > maxViolation:
-        value = minViolation
-    else:
-        value = maxViolation
+    @staticmethod
+    def dataNormalization(training, validation):
+        normalizerTraining = Normalization(axis=-1)
+        normalizerTraining.adapt(training)
+        normalizerValidation = Normalization(axis=-1)
+        normalizerValidation.adapt(validation)
 
-    return value
+        return normalizerTraining(training), normalizerValidation(validation)
 
+    @staticmethod
+    def evaluateSet(set, params):
+        global evaluated
+        evaluated = []
+        for x in set:
+            val = [0]
+            params.evaluationFunction(x, val, dimensions, 1, funNumCEC)
+            evaluated.append(val)
+        return np.array(evaluated)
 
-def penalty(vector, params):
-    arr = np.array([ params.penaltyFactor * max( 0, constraintViolation(x) ) for x in vector ])
-    penaltyValue = arr.sum()
-    return penaltyValue
+    @staticmethod
+    def evaluateWithModel(y, x, population, model, params):
+        x_val = model.predict(np.array(x).reshape(1,dimensions))
+        print("Model: " + str(x_val))
+        x_func = [0]
+        params.evaluationFunction(x, x_func, dimensions, 1, funNumCEC)
+        print("Funkcja: " + str(x_func))
 
+        y_val = model.predict(np.array(y).reshape(1,dimensions))
+        print("Model: " + str(y_val))
+        y_func = [0]
+        params.evaluationFunction(y, y_func, dimensions, 1, funNumCEC)
+        print("Funkcja: " + str(y_func))
 
-def evaluate(y, x, population, params):
-    global dimensions
-    global funNumCEC
-    x_val = [0]
-    y_val = [0]
-    params.evaluationFunction(x, x_val, dimensions, 1, funNumCEC)
-    params.evaluationFunction(y, y_val, dimensions, 1, funNumCEC)
-    x_val += penalty(x, params)
-    y_val += penalty(y, params)
-    if y_val <= x_val:
-        population[np.where( population == x )[0][0]] = y
-
-
-
-
-
-def DE(params):
-    global generationNum
-    population = initialization(params.populationSize)
-    fes = 0
-    while fes < params.maxfes:
-        for specimen in population:
-            individuals = generate(population, np.where( population == specimen )[0][0], params)
-            donorVector = mutation(individuals, params)
-            trialVector = crossover(specimen, donorVector, params)
-            evaluate(trialVector, specimen, population, params)
-            fes+=1
-        generationNum+=1
-    return population
-
-
-
-
-
-
-class DEANNParams:
-    populationSize = 50
-    crossoverProbability = 0.7
-    differentialWeight = 0.8
-    penaltyFactor = 0.1
-    maxfes = 1000
-    trainingDataSize = 10000
-    evaluationFunction = staticmethod(cec17_test_func)
-
-def generateTrainingData(params):
-    data = np.random.uniform(minValue, maxValue, (params.trainingDataSize, dimensions))
-    # lhsData = lhs(dimensions, samples=params.trainingDataSize)
-    # print(lhsData)
-    # data = np.array(lhsData) * 200
-    # data = data - 100
-    cut = np.int32(params.trainingDataSize * 0.8)
-    return data[:cut, :], data[cut:, :]
-
-
-def dataNormalization(training, validation):
-    normalizerTraining = Normalization(axis=-1)
-    normalizerTraining.adapt(training)
-    normalizerValidation = Normalization(axis=-1)
-    normalizerValidation.adapt(validation)
-
-    return normalizerTraining(training), normalizerValidation(validation)
-
-
-def evaluateSet(set, params):
-    val = [0]
-    global evaluated
-    evaluated = []
-    for x in set:
-        params.evaluationFunction(x, val, dimensions, 1, funNumCEC)
-        evaluated.append(val)
-    return np.array(evaluated)
-
-def evaluateWithModel(y, x, population, model, params):
-    x_val = model.predict(np.array(x).reshape(1,dimensions))
-    print("Model: " + str(x_val))
-    x_func = [0]
-    params.evaluationFunction(x, x_func, dimensions, 1, funNumCEC)
-    print("Funkcja: " + str(x_func))
-
-    y_val = model.predict(np.array(y).reshape(1,dimensions))
-    print("Model: " + str(y_val))
-    y_func = [0]
-    params.evaluationFunction(y, y_func, dimensions, 1, funNumCEC)
-    print("Funkcja: " + str(y_func))
-
-    x_val += penalty(x, params)
-    y_val += penalty(y, params)
-    if y_val <= x_val:
-        population[np.where( population == x )[0][0]] = y
+        x_val += Common.penalty(x, params)
+        y_val += Common.penalty(y, params)
+        if y_val <= x_val:
+            population[np.where( population == x )[0][0]] = y
 
 
 
-def DEANN(params):
-    global generationNum
-    population = initialization(params.populationSize)
+    def run(self, params):
+        global generationNum
+        population = Common.initialization(params.populationSize)
 
 
-    training, validation = generateTrainingData(params)
-    # normTraining, normValidation = dataNormalization(training, validation)
-    normalizer = Normalization()
-    normalizer.adapt(training)
+        training, validation = DEANN.generateTrainingData(params)
+        # normTraining, normValidation = dataNormalization(training, validation)
+        normalizer = Normalization()
+        normalizer.adapt(training)
 
-    evaluatedTraining = evaluateSet(training, params)
-    evaluatedValidation = evaluateSet(validation, params)
+        evaluatedTraining = DEANN.evaluateSet(training, params)
+        evaluatedValidation = DEANN.evaluateSet(validation, params)
 
-    inputs = keras.Input(shape=(dimensions))
+        inputs = keras.Input(shape=(dimensions))
 
-    # x = normalizer(inputs)
-    x = layers.Dense(20, activation=keras.activations.relu)(inputs)
-    x = layers.Dense(10, activation=keras.activations.relu)(x)
-    x = layers.Dense(2, activation=keras.activations.relu)(x)
+        # x = normalizer(inputs)
+        x = layers.Dense(20, activation=keras.activations.relu)(inputs)
+        x = layers.Dense(10, activation=keras.activations.relu)(x)
+        x = layers.Dense(2, activation=keras.activations.relu)(x)
 
-    outputs = layers.Dense(1, activation=keras.activations.linear)(x)
+        outputs = layers.Dense(1, activation=keras.activations.linear)(x)
 
-    model = keras.Model(inputs=inputs, outputs=outputs)
+        model = keras.Model(inputs=inputs, outputs=outputs)
 
-    model.compile(optimizer=keras.optimizers.Adam(), loss=keras.losses.MeanSquaredError())
+        model.compile(optimizer=keras.optimizers.Adam(), loss=keras.losses.MeanSquaredError())
 
-    model.fit(training, evaluatedTraining, epochs=1000, validation_data=(validation, evaluatedValidation))
-
-
-    fes = 0
-    while fes < params.maxfes:
-        for specimen in population:
-            individuals = generate(population, np.where( population == specimen )[0][0], params)
-            donorVector = mutation(individuals, params)
-            trialVector = crossover(specimen, donorVector, params)
-            evaluateWithModel(trialVector, specimen, population, model, params)
-            fes += 1
-        generationNum += 1
-    return population
+        model.fit(training, evaluatedTraining, epochs=2, validation_data=(validation, evaluatedValidation))
 
 
+        fes = 0
+        while fes < params.maxfes:
+            for specimen in population:
+                individuals = Common.generate(population, np.where( population == specimen )[0][0], params)
+                donorVector = Common.mutation(individuals, params)
+                trialVector = Common.crossover(specimen, donorVector, params)
+                DEANN.evaluateWithModel(trialVector, specimen, population, model, params)
+                fes += 1
+            generationNum += 1
+        return population
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
     global minValue
@@ -212,24 +189,28 @@ if __name__ == '__main__':
     global dimensions
     global generationNum
     global funNumCEC
-    minValue = -100  #cannot find what are the limits for this function
+    minValue = -100
     maxValue = 100
     generationNum = 0
     dimensions = 10  #only: 2, 10, 20, 30, 50, 100
     funNumCEC = 1
 
-    # random.seed(42)
-    # np.random.seed(42)
+    random.seed(42)
+    np.random.seed(42)
 
-    # dEParams = DEParams()
-    # population = DE(dEParams)
+    params = DEParams()
+
+
+
+    # DE_alg = DE()
+    # population = DE_alg.run(params)
     # print(population)
     # best = population[0]
     # best_val = [0]
-    # dEParams.evaluationFunction(best, best_val, dimensions, 1, funNumCEC)
+    # params.evaluationFunction(best, best_val, dimensions, 1, funNumCEC)
     # for s in population:
     #     s_val = [0]
-    #     dEParams.evaluationFunction(s, s_val, dimensions, 1, funNumCEC)
+    #     params.evaluationFunction(s, s_val, dimensions, 1, funNumCEC)
     #     if s_val <= best_val:
     #         best = s
     #         best_val = s_val
@@ -241,16 +222,15 @@ if __name__ == '__main__':
     # print("Numer generacji:" + str(generationNum))
 
 
-
-    dEANNParams = DEANNParams()
-    population = DEANN(dEANNParams)
+    DEANN_alg = DEANN()
+    population = DEANN_alg.run(params)
     print(population)
     best = population[0]
     best_val = [0]
-    dEANNParams.evaluationFunction(best, best_val, dimensions, 1, funNumCEC)
+    params.evaluationFunction(best, best_val, dimensions, 1, funNumCEC)
     for s in population:
         s_val = [0]
-        dEANNParams.evaluationFunction(s, s_val, dimensions, 1, funNumCEC)
+        params.evaluationFunction(s, s_val, dimensions, 1, funNumCEC)
         if s_val <= best_val:
             best = s
             best_val = s_val
@@ -262,6 +242,3 @@ if __name__ == '__main__':
     print("Numer generacji:" + str(generationNum))
 
 
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
