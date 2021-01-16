f510df8d887f4290c563fca48e2667a30bd15fe0
kkulesz <kulesza.konrad@protonmail.com>
2021-01-16 14:43:35 +0100

    refactorization

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import random
-#from sklearn.preprocessing import MinMaxScaler
-#import pyDOE
 from pyDOE import lhs
 import math
 import numpy as np
@@ -20,12 +13,6 @@ import matplotlib.pyplot as plt
 
 
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
 def func(specimen, valueToReturn, numOfDimensions, number, functionNumber):
     if functionNumber == 0:
         valueToReturn[0] = sum(100.0 * np.power( (np.subtract(specimen[1:], np.power(specimen[:-1], 2)) ), 2) + np.power((np.subtract(1, specimen[:-1])), 2))
@@ -34,7 +21,7 @@ def func(specimen, valueToReturn, numOfDimensions, number, functionNumber):
 
 class DEParams:
     populationSize = 100
-    crossoverProbability = 0.7
+    crossoverProbability = 0.5
     differentialWeight = 0.8
     maxfes = 2000
 
@@ -156,39 +143,11 @@ class DEANN:
     @staticmethod
     def evaluateWithModel(y, x, population, model, params):
         x_val = model.predict(np.array(x).reshape(1,dimensions))
-        # print("Model: " + str(x_val))
-        # x_func = [0]
-        # params.evaluationFunction(x, x_func, dimensions, 1, funNumCEC)
-        # print("Funkcja: " + str(x_func))
 
         y_val = model.predict(np.array(y).reshape(1,dimensions))
-        # print("Model: " + str(y_val))
-        # y_func = [0]
-        # params.evaluationFunction(y, y_func, dimensions, 1, funNumCEC)
-        # print("Funkcja: " + str(y_func))
 
         x_val += Common.penalty(x, params)
         y_val += Common.penalty(y, params)
-
-
-        # plotting
-        # global log
-        # global fig
-        # global ax
-        #
-        # newValue = pd.DataFrame({"x0":y[0],
-        #                          "x1":y[1],
-        #                          "y":y_val[0]})
-        #
-        # log = log.append(newValue, ignore_index=True)
-        #
-        # # log.plot(x= "x", y= "y", kind="scatter", ax=ax)
-        # # fig.show()
-        #
-        # ax.scatter3D(log.x0, log.x1, log.y)
-        # ax.set_zlim3d(0,10000)
-        # fig.show()
-
 
         if y_val <= x_val:
             population[np.where( population == x )[0][0]] = y
@@ -260,13 +219,12 @@ if __name__ == '__main__':
     funNumCEC = 1
 
     params = DEParams()
-
+    iters = 10
 
     DE_alg = DE()
-    for i in range(25):
-        print("#################################### "+ str(i+1)+ " #############################")
+    for i in range(iters):
+        print("##### "+ str(i+1)+ " #####")
         population = DE_alg.run(params)
-        #print(population)
         best = population[0]
         best_val = [0]
         params.evaluationFunction(best, best_val, dimensions, 1, funNumCEC)
@@ -276,21 +234,18 @@ if __name__ == '__main__':
             if s_val <= best_val:
                 best = s
                 best_val = s_val
-        #print("Najlepszy: ")
-        #print( str(best))
+        #print("Najlepszy: " +str(best) )
         #print("Wartość: ")
         print(best_val)
-
         #print("Numer generacji:" + str(generationNum))
 
 
     DEANN_alg = DEANN()
     model = DEANN_alg.getModel(params)
 
-    for i in range(25):
-        print("#################################### "+ str(i+1)+ " #############################")
+    for i in range(iters):
+        print("##### "+ str(i+1)+ " #####")
         population = DEANN_alg.run(params, model)
-        #print(population)
         best = population[0]
         best_val = [0]
         params.evaluationFunction(best, best_val, dimensions, 1, funNumCEC)
@@ -300,11 +255,9 @@ if __name__ == '__main__':
             if s_val <= best_val:
                 best = s
                 best_val = s_val
-        #print("Najlepszy: ")
-        #print(str(best))
+        #print("Najlepszy: " + str(best) )
         #print("Wartość: ")
         print(best_val)
-
         #print("Numer generacji:" + str(generationNum))
 
 
